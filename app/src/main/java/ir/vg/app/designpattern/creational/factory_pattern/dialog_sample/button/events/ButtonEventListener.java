859e1a43b524a9535fda71fae32748c944c0ba78
Vahid Garousi <garousiv@gmail.com>
2021-11-22 10:55:09 +0330

    Fixes #8 -> Deep dive into factory pattern with Dialog example

---
+++ app/src/main/java/ir/vg/app/designpattern/creational/factory_pattern/dialog_sample/button/events/ButtonEventListener.java
@@ -0,0 +1,6 @@
+package ir.vg.app.designpattern.creational.factory_pattern.dialog_sample.button.events;
+
+public interface ButtonEventListener {
+    void setOnClickListener();
+    void setOnLongClickListener();
+}
